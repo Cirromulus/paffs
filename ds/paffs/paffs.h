546368edc33a7dcd020647c688bbd91fbf834290
Pascal Pieper <Pascal.Pieper@dlr.de>
2016-09-02 13:32:15 +0000

    Added some detail to documentation

--- ds/paffs/paffs.h
+++ ds/paffs/paffs.h
@@ -44,7 +44,7 @@ static const char* PAFFS_RESULT_MSG[] = {
 		"You should not be seeing this..."
 };
 
-typedef enum pinode_type{
+typedef enum pInode_type{
     PINODE_FILE,
     PINODE_DIR,
     PINODE_LNK
@@ -98,16 +98,16 @@ typedef uint32_t dirEntryLength;
 typedef struct pInode{
 	pInode_no no;
 	//unsigned int seq_no;	//not used yet
-	pinode_type type:3;
+	pInode_type type:3;
 	paffs_permission perm:3;
 	p_date crea;
 	p_date mod;
 	unsigned long long reservedSize;
 	unsigned long long size;    //~1,8 * 10^19 Byte
 	p_addr direct[11];
-	//p_addr indir;
-	//p_addr d_indir;
-	///p_addr t_indir;
+	p_addr indir;
+	p_addr d_indir;
+	p_addr t_indir;
 } pInode;
 
 
