161e1aeaf18c9f65d6d1a440e3850b04c9f756da
Pascal Pieper <Pascal.Pieper@dlr.de>
2016-12-28 22:07:00 +0000

    First Garbage collection functions

---
+++ ds/paffs/garbage_collection.c
@@ -0,0 +1,13 @@
+/*
+ * garbage_collection.c
+ *
+ *  Created on: 28.12.2016
+ *      Author: urinator
+ */
+
+#include "garbage_collection.h"
+
+PAFFS_RESULT collectGarbage(p_dev* dev){
+
+	return PAFFS_NIMPL;
+}
---
+++ ds/paffs/garbage_collection.h
@@ -0,0 +1,15 @@
+/*
+ * garbage_collection.h
+ *
+ *  Created on: 28.12.2016
+ *      Author: urinator
+ */
+
+#ifndef DS_PAFFS_GARBAGE_COLLECTION_H_
+#define DS_PAFFS_GARBAGE_COLLECTION_H_
+
+#include "paffs.h"
+
+PAFFS_RESULT collectGarbage(p_dev* dev);
+
+#endif /* DS_PAFFS_GARBAGE_COLLECTION_H_ */
