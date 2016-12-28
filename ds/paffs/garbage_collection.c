8b45f0facb5cb8dc2a12002a7acab51446ef3cc8
Pascal Pieper <Pascal.Pieper@dlr.de>
2016-12-28 23:40:46 +0000

    Nicht viel weiter

--- ds/paffs/garbage_collection.c
+++ ds/paffs/garbage_collection.c
@@ -7,7 +7,18 @@
 
 #include "garbage_collection.h"
 
-PAFFS_RESULT collectGarbage(p_dev* dev){
+uint32_t countDirtyPages(p_summaryEntry* summary){
+	return -1;
+}
 
+PAFFS_RESULT collectGarbage(p_dev* dev){
+	uint32_t favourite = 0;
+	for(uint32_t i = 0; i < dev->param.areas_no; i++){
+		if(dev->areaMap[favourite].status == CLOSED && dev->areaMap[favourite].type != SUPERBLOCKAREA){
+			if(dev->areaMap[favourite].has_areaSummary){
+				//TODO: 'has_areaSummary' means just not cached, should get it from Flash
+			}
+		}
+	}
 	return PAFFS_NIMPL;
 }
