948ca7f62448b021f122a4c5c0a5f7cb9004178f
Pascal Pieper <Pascal.Pieper@dlr.de>
2017-08-28 13:55:17 +0200

    Removed dead comments

--- it/logic/config/simu/config.hpp
+++ it/logic/config/simu/config.hpp
@@ -9,12 +9,11 @@
 
 
 #define DATA_TYPE unsigned char
-#define PAGE_DATA 512 //1024
+#define PAGE_DATA 512
 #define PAGE_AUX (PAGE_DATA / 32)
 #define PAGE_SIZE (PAGE_DATA + PAGE_AUX)
-#define BLOCK_SIZE 64	//128
-#define PLANE_SIZE 8	//8
-//#define CELL_SIZE 1024
-#define CELL_SIZE 8	//4
+#define BLOCK_SIZE 64
+#define PLANE_SIZE 8
+#define CELL_SIZE 8
 
 #define TID_FLIP_START_PERCENT 0.85
