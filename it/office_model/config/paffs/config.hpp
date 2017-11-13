db2a2e0ed31addaf50d7d8c220dd618b8e4977a4
Pascal Pieper <Pascal.Pieper@dlr.de>
2017-11-13 15:43:01 +0100

    Office model IT now uses own config

---
+++ it/office_model/config/paffs/config.hpp
@@ -0,0 +1,28 @@
+/*
+ * standard.hpp
+ *
+ *  Created on: Feb 14, 2017
+ *      Author: Pascal Pieper
+ */
+#include <stdint.h>
+#pragma once
+
+namespace paffs{
+	//Flash config
+	static constexpr uint16_t dataBytesPerPage  = 4096;
+	static constexpr uint8_t  oobBytesPerPage = 128;
+	static constexpr uint16_t pagesPerBlock = 64;
+	static constexpr uint16_t blocksTotal = 4096;
+	static constexpr uint8_t  blocksPerArea = 4;
+	static constexpr uint8_t  jumpPadNo = 3;				//Should scale with max(0, log2(blocks / 32))
+
+	//MRam config
+	static constexpr uint32_t mramSize = 4096 * 512;		//Should be a multiple of 512 for viewer
+
+	//Cache sizes
+	static constexpr uint8_t  treeNodeCacheSize = 5;		//max. 1,5 * dataBytesPerPage(TreeNode) Bytes per Entry
+	static constexpr uint8_t  areaSummaryCacheSize = 4;		//Currently  2 Bit per dataPagesPerArea
+	static constexpr uint8_t  maxNumberOfDevices = 2;
+	static constexpr uint8_t  maxNumberOfInodes = 10;		//limits simultaneously open files/folders excluding duplicates
+	static constexpr uint8_t  maxNumberOfFiles = 10;		//limits simultaneously open files including duplicates
+}
